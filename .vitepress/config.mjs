3bbf1f19d492d737a0993dafa07f1303c2c42db8
潘志清 <82391775+pzq123456@users.noreply.github.com>
2024-12-12 21:34:12 +0800

    fix: 更新网站标题为“Global EV Data Initiative”，以更好地反映项目目标

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { viteConfig } from './viteConfig'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Global EV Data",
+  title: "Global EV Data Initiative",
   description: "An open electric vehicle data initiative for sharing the latest and historical information on the electric vehicle market, policy and charging infrastructure across the globe.",
   themeConfig: themeConfig,
   base: '/EVChargerSite',
